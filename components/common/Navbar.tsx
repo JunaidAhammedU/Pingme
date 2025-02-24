879d38177af004cd2661dabb00185f5c3549eb34
JunaidAhammedU <junaidahammed15@gmail.com>
2025-02-24 11:38:26 +0530

    Replace SignOutButton with UserButton in Navbar component

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -1,14 +1,10 @@
-import { SignOutButton } from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Navbar() {
   return (
     <nav className="bg-gray-200 p-4">
       <div className="container mx-auto flex justify-end">
-        <SignOutButton>
-          <button className="bg-red-500 text-white p-2 rounded">
-            Sign Out
-          </button>
-        </SignOutButton>
+        <UserButton />
       </div>
     </nav>
   );
